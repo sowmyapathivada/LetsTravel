0420c1a942d9198b05eeb6d96d88f647eb6bfc60
SOWMYAPATHIVADA <sowmyapathivada29@gmail.com>
2022-11-08 17:08:23 +0530

    let's travel data

---
+++ deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "C:/Users/sowmyap/Documents/Mendix/LETS TRAVEL-main_2/themesource/administration/native/main";
+import * as style1 from "C:/Users/sowmyap/Documents/Mendix/LETS TRAVEL-main_2/themesource/excelimporter/native/main";
+import * as style2 from "C:/Users/sowmyap/Documents/Mendix/LETS TRAVEL-main_2/themesource/nanoflowcommons/native/main";
+import * as style3 from "C:/Users/sowmyap/Documents/Mendix/LETS TRAVEL-main_2/themesource/webactions/native/main";
+import * as style4 from "C:/Users/sowmyap/Documents/Mendix/LETS TRAVEL-main_2/themesource/atlas_core/native/main";
+import * as style5 from "C:/Users/sowmyap/Documents/Mendix/LETS TRAVEL-main_2/themesource/lets_travel/native/main";
+import * as style6 from "C:/Users/sowmyap/Documents/Mendix/LETS TRAVEL-main_2/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
